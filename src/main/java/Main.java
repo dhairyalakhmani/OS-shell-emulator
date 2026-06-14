43093b15b8af38d332e10d2d47bf89ba01fd0f0b
dhairyalakhmani <dhairyalakhmani27@gmail.com>
2026-06-14 20:09:13 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,14 @@
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
+        Set<String> set = new HashSet<>();
+        set.add("exit");
+        set.add("echo");
+        set.add("type");
         while (true) {
             System.out.print("$ ");
             String input = sc.nextLine().trim();
@@ -12,6 +18,10 @@ public class Main {
             String arguments = parts.length > 1 ? parts[1] : "";
             if (command.equals("exit")) break;
             else if (command.equals("echo")) System.out.println(arguments);
+            else if(command.equals("type")){
+                if(set.contains(arguments)) System.out.println(arguments + " is a shell builtin");
+                else System.out.println(arguments + ": not found");
+            }
             else System.out.println(command + ": command not found");
         }
     }
