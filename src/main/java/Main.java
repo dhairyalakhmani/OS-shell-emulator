b1dc20315b442c703407b25c5de496885d396131
dhairyalakhmani <dhairyalakhmani27@gmail.com>
2026-06-18 02:28:00 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.*;
 
 public class Main {
@@ -12,13 +13,29 @@ public class Main {
             System.out.print("$ ");
             String input = sc.nextLine().trim();
             if (input.isEmpty()) continue;
+            String targetFilePath = "";
+            boolean isAppend = false, isError = false, foundPath = false;
             List<String> parsedInput = parseInput(input);
+            Set<String> validSTDOperators = new HashSet<>();
+            validSTDOperators.add(">"); validSTDOperators.add(">>"); validSTDOperators.add("1>"); validSTDOperators.add("1>>"); validSTDOperators.add("2>"); validSTDOperators.add("2>>");
+            for(int i = parsedInput.size() - 1; i >= 0; i--){
+                String currentToken = parsedInput.get(i);
+                if(validSTDOperators.contains(currentToken)){
+                    targetFilePath = parsedInput.get(i + 1);
+                    foundPath = true;
+                    isAppend = currentToken.endsWith(">>");
+                    isError = currentToken.startsWith("2");
+                    parsedInput.remove(i + 1);
+                    parsedInput.remove(i);
+                    break;
+                }
+            }
             String command = parsedInput.get(0);
             String arguments = parsedInput.size() > 1 ? parsedInput.get(1) : "";
             if (command.equals("exit")) break;
-            else if (command.equals("echo")) System.out.println(String.join(" ", parsedInput.subList(1, parsedInput.size())));
+            else if (command.equals("echo")) writeOutput(String.join(" ", parsedInput.subList(1, parsedInput.size())), foundPath, targetFilePath, isAppend, isError);
             else if(command.equals("pwd")){
-                System.out.println(System.getProperty("user.dir"));
+                writeOutput(System.getProperty("user.dir"), foundPath, targetFilePath, isAppend, isError);
             }
             else if(command.equals("cd")){
                 Path targetPath;
@@ -36,43 +53,63 @@ public class Main {
                 if(Files.exists(targetPath) && Files.isDirectory(targetPath)){
                     System.setProperty("user.dir", targetPath.normalize().toString());
                 }else{
-                    System.out.println("cd: " + arguments + ": No such file or directory");
+                    writeOutput("cd: " + arguments + ": No such file or directory", foundPath, targetFilePath, isAppend, isError);
                 }
             }
             else if(command.equals("type")){
-                if(set.contains(arguments)) System.out.println(arguments + " is a shell builtin");
+                if(set.contains(arguments)) writeOutput(arguments + " is a shell builtin", foundPath, targetFilePath, isAppend, isError);
                 else {
                     String path = System.getenv("PATH");
                     if(path != null && !path.isEmpty()){
                         String[] directories = path.split(File.pathSeparator);
-                        hasPath(directories, arguments);
+                        hasPath(directories, arguments, foundPath, targetFilePath, isAppend, isError);
                     }
                 }
             }
             else{
                 ProcessBuilder processBuilder = new ProcessBuilder(parsedInput);
                 processBuilder.inheritIO();
+                if (foundPath) {
+                    File myFile = new File(targetFilePath);
+                    ProcessBuilder.Redirect redirect;
+                    if (isAppend) redirect = ProcessBuilder.Redirect.appendTo(myFile);
+                    else redirect = ProcessBuilder.Redirect.to(myFile);
+                    if (isError) processBuilder.redirectError(redirect);
+                    else processBuilder.redirectOutput(redirect);
+                }
                 try{
                     Process process = processBuilder.start();
                     process.waitFor();
                 } catch(java.io.IOException e){
-                    System.out.println(command + ": command not found");
+                    writeOutput(command + ": command not found", foundPath, targetFilePath, isAppend, isError);
                 }
             }
         }
     }
-    private static void hasPath(String[] directories, String arguments){
+
+    private static void writeOutput(String content, boolean foundPath, String targetFilePath, boolean isAppend, boolean isError) throws Exception {
+        if (foundPath && !isError) {
+            Path path = Path.of(targetFilePath);
+            if (isAppend) Files.writeString(path, content + "\n", StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+            else Files.writeString(path, content + "\n", StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+        } else {
+            System.out.println(content);
+        }
+    }
+
+    private static void hasPath(String[] directories, String arguments, boolean foundPath, String targetFilePath, boolean isAppend, boolean isError) throws Exception {
         boolean found = false;
         for(String dir: directories){
             Path fullPath = Path.of(dir, arguments);
             if(Files.exists(fullPath) && Files.isExecutable(fullPath)){
-                System.out.println(arguments + " is " + fullPath.toString());
+                writeOutput(arguments + " is " + fullPath.toString(), foundPath, targetFilePath, isAppend, isError);
                 found = true;
                 break;
             }
         }
-        if(!found) System.out.println(arguments + ": not found");
+        if(!found) writeOutput(arguments + ": not found", foundPath, targetFilePath, isAppend, isError);
     }
+
     private static List<String> parseInput(String input){
         List<String> tokens = new ArrayList<>();
         StringBuilder currentToken = new StringBuilder();
